c1f100949370df208426241e947118f22ab4fd85
faco400 <vd.souza1999@gmail.com>
2023-06-13 20:14:47 -0300

    feature: Iniciando middleware
    Co-authored-by: gabrielapivetta <gpivetta99@gmail.com>

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from lib.dao.database import initialize_database
 
 from api.user.controller import users
@@ -7,10 +7,12 @@ from api.station_address.controller import stations
 
 from fastapi.middleware.cors import CORSMiddleware
 
+import requests
+
 api = FastAPI()
 
 origins = [
-    "http://localhost:3000",
+    "*",
 ]
 
 api.add_middleware(
@@ -24,6 +26,30 @@ api.add_middleware(
 api.include_router(users)
 api.include_router(cars)
 api.include_router(stations)
+
+endpoint = ("https://api.eletrogama.online/login")
+
+@api.middleware("http")
+async def teste(request: Request, call_next):
+    print("Verificação do token")
+
+    print(request.headers)
+
+    auth_token = request.headers.get('authorization')
+    
+    if (auth_token):
+        bearer_token: str = auth_token.split('Bearer')[1].strip()
+        headers = {"authorization": "Bearer " + bearer_token}
+
+        token_validado = auth.verify_id_token(auth_token)
+
+        print('Token',token_validado)
+
+    return await call_next(request)
+    # print("middleware retornar usuario fazendo request")
+    # print(request.auth)
+    # response = await call_next(request)
+    # return response
 
 @api.on_event("startup")
 async def startup():
